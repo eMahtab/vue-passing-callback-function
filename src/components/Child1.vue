aecf7aaf87ffc41fcdf61320792ee0d2f50dbbd8
Mahtab Alam <alammahtab08@gmail.com>
2018-04-21 09:47:39 +0530

    Example showing how to pass callback function from parent component to child component using props

---
+++ src/components/Child1.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="child1" class="component">
+    
+    <h2>Child 1 Component</h2>
+    <span>{{message}}</span>
+    <br/><br/>
+    <button @click="changeMessage">Change Message</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message:{String},
+    changeMessage: Function
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#child1 {
+  background-color: lightseagreen;
+  
+}
+</style>
---
+++ src/components/Parent.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="parent" class="component">
+    
+    <h2>Parent Component</h2>
+    <span>{{msg}}</span>
+      <hr/>
+      <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <child1 :message="msg" :changeMessage="updateMessage"></child1>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <child2 :message="msg"></child2>
+            </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import Child1 from './Child1.vue';
+import Child2 from './Child2.vue';
+
+export default {
+  components:{child1:Child1, child2:Child2},
+  data () {
+    return {
+      msg: 'Our lives begin to end the day we become silent about things that matter - Martin Luther King, Jr.'
+    }
+  },
+  methods: {
+    updateMessage(){
+      this.msg = "A day of worry is more exhausting than a week of work - John Lubbock"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#parent {
+  background-color: dodgerblue;
+  
+}
+
+
+
+</style>
